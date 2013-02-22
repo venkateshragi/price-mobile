7a5211f4fb41054d6035c805bc223fe1050faddf
Venkatesh Ragi <venkateshragi@gmail.com>
2013-02-22 12:40:41 +0530

    Refactored code

--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
 var app = {
 
 	onLoad : function() {
-		$('body').load(function(){
-			setInterval(makeAjaxCall, 3000);
+		$(window).load(function(){
+			setInterval(function() {
+				$.getJSON("http://svbcgold.com/LivePriceService.asmx/getSVBCPrice", function(response){
+					var amountToBeAddedForGold = properties.addAmountForGold;
+					var amountToBeAddedForSilver = properties.addAmountForSilver;
+					var pricePerGm = (response.SpotGold999 * 1) + amountToBeAddedForGold;
+					document.getElementById("lblSpotGold999").innerHTML = pricePerGm;
+					document.getElementById("lblSpotSilver999").innerHTML = (response.SpotSilver999 * 1) + amountToBeAddedForSilver;
+					document.getElementById("lblSpotGold916").innerHTML = pricePerGm * 50;
+					document.getElementById("lblGold").innerHTML = (response.GoldCMX * 1) + amountToBeAddedForGold;
+					document.getElementById("lblSilver").innerHTML = (response.SilverCMX * 1) + amountToBeAddedForSilver;
+					document.getElementById("lblUsd").innerHTML = (response.USDInr * 1);			
+				})}, 3000);
 		});			
-	},
-		
-	makeAjaxCall : function() {
-		$.getJSON("http://svbcgold.com/LivePriceService.asmx/getSVBCPrice", function(response){
-			var amountToBeAddedForGold = properties.addAmountForGold;
-			var amountToBeAddedForSilver = properties.addAmountForSilver;
-			var pricePerGm = (response.SpotGold999 * 1) + amountToBeAddedForGold;
-			document.getElementById("lblSpotGold999").innerHTML = pricePerGm;
-			document.getElementById("lblSpotSilver999").innerHTML = (response.SpotSilver999 * 1) + amountToBeAddedForSilver;
-			document.getElementById("lblSpotGold916").innerHTML = pricePerGm * 50;
-			document.getElementById("lblGold").innerHTML = (response.GoldCMX * 1) + amountToBeAddedForGold;
-			document.getElementById("lblSilver").innerHTML = (response.SilverCMX * 1) + amountToBeAddedForSilver;
-			document.getElementById("lblUsd").innerHTML = (response.USDInr * 1);			
-		});
-	}
+	}	
 }
 
 app.onLoad();
